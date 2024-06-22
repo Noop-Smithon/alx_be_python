0a5e9eeb47aec33f1f17448097a2e9391058e23b
Princewill Smithon <onumaduprincewill@gmail.com>
2024-06-22 14:25:38 +0100

    Update future_age_calculator.py File

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -1,4 +1,4 @@
-current_age = int(input("How old are you?: "))
+current_age = int(input("How old are you? "))
 current_year = 2023
 future_year = 2050
 future_age = (future_year - current_year) + current_age
