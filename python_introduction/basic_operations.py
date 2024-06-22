ea474a8cdf76dd35ba9135286c9254ea016d9c4b
Princewill Smithon <onumaduprincewill@gmail.com>
2024-06-22 13:08:34 +0100

    Create basic_operations.py file and Carry Out Basic Arithmetic Operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,11 @@
+number1 = 10
+number2 = 5
+sum = number1 + number2
+diff = number1 - number2
+product = number1 * number2
+sum_result = f"Addition of {number1} and {number2} is {sum}"
+diff_result = f"Subtraction of {number1} and {number2} is {diff}"
+product_result = f"Multiplication of {number1} and {number2} is {product}"
+print(sum_result)
+print(diff_result)
+print(product_result)
